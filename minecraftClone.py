27ef63ace0887ef6e2787e3afb9a315ea89d6350
ZippyCode <durusst8@gmail.com>
2021-07-11 10:28:13 +0200

    arm movement

--- minecraftClone.py
+++ minecraftClone.py
@@ -1,26 +1,39 @@
 from ursina import *
 from ursina.prefabs.first_person_controller \
-import FirstPersonController
+  import FirstPersonController
 
 app = Ursina()
 Sky()
 player = FirstPersonController()
 
+arm = Entity(
+  parent=camera.ui,
+  model='cube',
+  color=color.blue,
+  position=(0.75, -0.6),
+  rotation= (150, -10,6),
+  scale = (0.2,0.2,1.5)
+)
+def update():
+  if held_keys['left mouse']:
+    arm.position = (0.6, -0.5)
+  elif held_keys['right mouse']:
+    arm.position = (0.6, -0.5)
+  else:
+    arm.position = (0.75, -0.6)
 
-
-window.fullscreen = True
 boxes = []
 
 for n in range(15):
   for k in range(15):
     box = Button(
-      position= (k,0,n),
-      color= color.orange,
-      highlight_color = color.lime,
-      model= 'cube',
-      texture= 'white_cube',
-      origin_y= 0.5,
-      parent= scene
+      position=(k, 0, n),
+      color=color.orange,
+      highlight_color=color.lime,
+      model='cube',
+      texture='white_cube',
+      origin_y=0.5,
+      parent=scene
     )
     boxes.append(box)
 
@@ -30,7 +43,7 @@ def input(key):
     if box.hovered:
       if key == 'left mouse down':
         newBox = Button(
-          position= box.position+mouse.normal,
+          position=box.position + mouse.normal,
           color=color.orange,
           highlight_color=color.lime,
           model='cube',
@@ -43,18 +56,4 @@ def input(key):
         boxes.remove(box)
         destroy(box)
 
-
-
 app.run()
-
-
-
-
-
-
-
-
-
-
-
-
