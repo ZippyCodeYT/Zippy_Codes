1a6ba0ad2e53c8ba65121e29a5acc1e91767044c
ZippyCode <durusst8@gmail.com>
2021-08-01 22:38:50 +0200

    moving platforms, quit button

--- 3Dgame.py
+++ 3Dgame.py
@@ -1,9 +1,10 @@
 from ursina import *
 from ursina.prefabs.first_person_controller \
   import FirstPersonController
+from random import uniform
 
 app = Ursina()
-window.fullscreen = True
+
 ground = Entity(model= 'plane',
                 texture= 'grass',
                 collider= 'mesh',
@@ -20,4 +21,38 @@ myBall = Entity(model= 'sphere',
                 color= color.red,
                 collider= 'sphere',
                 position= (5, 0.5, 10))
+
+blocks = []
+directions = []
+
+for i in range(10):
+  r = uniform(-2,2)
+  block = Entity(
+    position=(r, 1 , 3+i*5),
+    model='cube',
+    texture='white_cube',
+    color=color.azure,
+    scale=(3, 0.5, 3),
+    collider='box',
+  )
+  blocks.append(block)
+  if r < 0:
+    directions.append(1)
+  else:
+    directions.append(-1)
+
+def update():
+  if player.y > 1:
+    destroy(ground)
+  i = 0
+  for block in blocks:
+    block.x -= directions[i] * time.dt
+    if abs(block.x) > 5:
+      directions[i] *= -1
+    i = i + 1
+
+def input(key):
+  if key == 'q':
+    quit()
+
 app.run()
